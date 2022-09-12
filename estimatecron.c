2bfcf97737d408ba3078f2c979cdef54bc8f0df1
Chowewow <chowewoow@gmail.com>
2022-09-12 20:54:24 +0800

    sams turn

--- estimatecron.c
+++ estimatecron.c
@@ -1,6 +1,6 @@
 //  CITS2002 Project 1 2022
 //  Student1:   22575612   Howe   Cody 
-//  Student2:   STUDENT-NUMBER2   Goodridge   Sam 
+//  Student2:   22700309   Goodridge   Sam 
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -12,11 +12,34 @@
 #define LINE_LEN        100
 #define COMMAND_LEN     40
 
+void trim_line(char line[])
+{
+    int i = 0;
+
+//  LOOP UNTIL WE REACH THE END OF line
+    while(line[i] != '\0') {
+
+//  CHECK FOR CARRIAGE-RETURN OR NEWLINE
+        if( line[i] == '\r' || line[i] == '\n'|| line[i] == ' ') {
+            line[i] = '\0'; // overwrite with null-byte
+            break;          // leave the loop early
+        }
+        i = i+1;            // iterate through character array
+    }
+}
+
+
 struct commands {
     char command[COMMAND_LEN];
     int time;
+    int min;
+    int hour;
+    int day;
+    int month;
+    int day_of_week;
 };
 
+int line_count;
 struct commands c1[COMMAND_LINES];
 
 void process_estimates(char estimatef[]) 
@@ -53,30 +76,33 @@ void process_estimates(char estimatef[])
         printf("%s %d\n", c1[line_index].command, c1[line_index].time);
         line_index++;
     }
+    line_count = line_index;
 }
 
 void process_cron(char cronf[]) 
 {
     FILE *cron = fopen(cronf, "r");
     char line[LINE_LEN];
-    printf("%s\n", c1[1].command);
-    char croncommand[COMMAND_LEN]; 
     while(fgets(line, sizeof(line), cron) != NULL)
     {
         if (line[0] == '#')
         {
             continue;
         }
-        printf("%s\n", line);
-        int i = strlen(line);
-
-
+        int i = 0;
+        while(i < line_count)
+        {
+            if(strnstr(line, c1[i].command, strlen(line)))
+            {
+                
+            }
+            i++;
+        }
     }
 }
 int main(int argc, char *argv[])
 {
     process_estimates(argv[2]);
     process_cron(argv[1]);
-    int k = 0;
 
 }
